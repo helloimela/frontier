fb7ff49756358387d278caea6c18e42edd487e26
Ela Yudhanira <elayudhanira@n180-p150.eduroam.kth.se>
2017-11-07 10:09:02 +0100

    Update pages

--- dev/js/main/app.js
+++ dev/js/main/app.js
@@ -5,8 +5,8 @@ var app = angular.module('frontier', [
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
   $routeProvider
     // Home
-    .when("/", {templateUrl: "partials/home.html", controller: "HomeCtrl", animation:"home"})
-    .when("/about", {templateUrl: "partials/about.html", controller: "AboutCtrl", animation:"about"});
+    .when("/", {templateUrl: "partials/home.html", controller: "HomeCtrl"})
+    .when("/species", {templateUrl: "partials/views/species.html", controller: "SpeciesCtrl"});
     // Pages
 
     $locationProvider.html5Mode({
@@ -15,7 +15,9 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
     });
 }]);
 
-app.controller('MainCtrl', ['$sce','$http','$scope','$location','$rootScope','$window',function($sce,$http, $scope, $location, $rootScope, $window){
+app.controller('MainCtrl', ['Page','$sce','$http','$scope','$location','$rootScope','$window',function(Page,$sce,$http, $scope, $location, $rootScope, $window){
+  $scope.Page = Page;
+
 	$scope.go = function ( path ) {
 	  $location.path( path );
 	};
@@ -23,7 +25,11 @@ app.controller('MainCtrl', ['$sce','$http','$scope','$location','$rootScope','$w
 }]);
 
 app.controller('HomeCtrl', ['$http','$location','$scope',function($http, $location, $scope, $rootScope){
+  Page.setTitle('Frontier - A Planet of a Thousand Footprints');
+}]);
 
+app.controller('SpeciesCtrl', ['Page','$http','$location','$scope',function(Page,$http, $location, $scope, $rootScope){
+  Page.setTitle('Species Overview');
 }]);
 
 app.directive('nav', ['$location',function($location){
@@ -32,3 +38,18 @@ app.directive('nav', ['$location',function($location){
     templateUrl:'partials/nav.html'
   };
 }]);
+
+app.directive('header', ['$location',function($location){
+  return{
+    restrict : 'E',
+    templateUrl:'partials/header.html'
+  };
+}]);
+
+app.factory('Page', function(){
+  var title = 'Frontier - A Planet of a Thousand Footprints';
+  return {
+    title: function() { return title; },
+    setTitle: function(newTitle) { title = newTitle; }
+  };
+});
