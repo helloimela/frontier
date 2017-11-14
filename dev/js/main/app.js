7f6eb819766fcbe1ecced439688a230964a04e5f
Ela Yudhanira <elayudhanira@n132-p104.eduroam.kth.se>
2017-11-14 13:11:50 +0100

    Subscribe to channel

--- dev/js/main/app.js
+++ dev/js/main/app.js
@@ -28,83 +28,114 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
   });
 }]);*/
 
-app.controller('MainCtrl', ['Page','$sce','$http','$scope','$location','$rootScope','$window',function(Page,$sce,$http, $scope, $location, $rootScope, $window){
+app.controller('MainCtrl', ['Pubnub','Page','$sce','$http','$scope','$location','$rootScope','$window',function(Pubnub,Page,$sce,$http, $scope, $location, $rootScope, $window){
   $scope.Page = Page;
 
 	$scope.go = function ( path ) {
 	  $location.path( path );
 	};
-}]);
+  $rootScope.messages = [];
+  $rootScope.presences=[];
 
-
-app.controller('ChatCtrl', ['Pubnub','Page','$http','$location','$scope',function(Pubnub,Page,$http, $location, $scope, $rootScope){
- $scope.channel = 'game-channel';
+  $scope.channel = 'game-channel';
   $scope.uuid = Math.random(100).toString();
   Pubnub.init({
    publish_key: 'pub-c-445d8ace-41a9-40d2-b68c-bdfcc1e2a298',
    subscribe_key: 'sub-c-33a57daa-c3a7-11e7-b683-b67c7dbcdd00',
    uuid: $scope.uuid
   });
-     // Send the messages over PubNub Network
-  $scope.sendMessage = function() {
-     // Don't send an empty message 
-     if (!$scope.messageContent || $scope.messageContent === '') {
-          return;
-      }
-      Pubnub.publish({
-          channel: $scope.channel,
-          message: {
-              content: $scope.messageContent,
-              sender_uuid: $scope.uuid,
-              date: new Date()
-          },
-          callback: function(m) {
-              console.log(m);
-          }
-      });
-      // Reset the messageContent input
-      $scope.messageContent = '';
-
-  };
-
-  $scope.messages = [];
 
   // Subscribing to the ‘messages-channel’ and trigering the message callback
   Pubnub.subscribe({
       channel: $scope.channel,
-      triggerEvents: ['callback']
+      triggerEvents: ['callback'],
+      presence:function(m){
+        console.log(m);
+        $rootScope.presences=m;
+      }
+  });
+
+  $scope.sendMessage =  function(){
+
+    Pubnub.publish({
+          channel: $scope.channel,
+          message: {
+              speciesId:$rootScope.userID,
+              sender_uuid: $scope.uuid
+          },
+          callback: function(m) {
+            console.log(m);
+          }
+      });
+    $location.path('/species/'+$scope.userID);
+  };
+
+  $scope.$on(Pubnub.getPresenceEventNameFor($scope.channel),function(e,payload){
+    console.log(e);   
+    console.log(payload);   
   });
 
   // Listening to the callbacks
   $scope.$on(Pubnub.getMessageEventNameFor($scope.channel), function (ngEvent, m) {
       $scope.$apply(function () {
-          $scope.messages.push(m);
+          $rootScope.messages.push(m);
+          console.log(ngEvent);
+          console.log(m);
       });
   });
+
+  $scope.sendReady = function(){
+    Pubnub.publish({
+          channel: $scope.channel,
+          message: {
+              ready:1
+          },
+          callback: function(m) {
+            // console.log(m);
+          }
+      });
+  };
+
 }]);
 
-app.controller('HomeCtrl', ['Page','$http','$location','$scope',function(Page,$http, $location, $scope, $rootScope){
+
+// app.controller('SubscribeCtrl', ['Pubnub','Page','$http','$location','$scope','$rootScope',function(Pubnub,Page,$http, $location, $scope, $rootScope){
+ 
+//      // Send the messages over PubNub Network
+//   // $scope.sendMessage = function() {
+//   //     Pubnub.publish({
+//   //         channel: $scope.channel,
+//   //         message: {
+//   //             // content: $scope.messageContent,
+//   //             sender_uuid: $scope.uuid,
+//   //             date: new Date()
+//   //         },
+//   //         callback: function(m) {
+//   //           console.log(m);
+//   //         }
+//   //     });
+//       // Reset the messageContent input
+//       // $scope.messageContent = '';
+
+//   // };
+// }]);
+
+app.controller('HomeCtrl', ['Page','$http','$location','$scope','$rootScope',function(Page,$http, $location, $scope, $rootScope){
   Page.setTitle('Frontier - A Planet of a Thousand Footprints');
 }]);
 
-app.controller('NewplayerCtrl', ['Page','$timeout','$http','$location','$scope',function(Page,$timeout,$http, $location, $scope, $rootScope){
+app.controller('NewplayerCtrl', ['Page','$timeout','$http','$location','$scope','$rootScope',function(Page,$timeout,$http, $location, $scope, $rootScope){
   Page.setTitle('Who are you?');
-  $scope.userID = Math.floor(Math.random() * 2) + 0;
-  console.log($scope.userID);
+  $rootScope.userID = Math.floor(Math.random() * 2) + 0;
   $scope.words = 'In the process of creating an awesome species ...';
   $timeout( function(){
-    if($scope.userID==0){
+    if($rootScope.userID==0){
       $scope.words = 'You are Space Ulk!';  
     } else {
       $scope.words = 'You are Space Feline!';
     }
     $scope.showButton = true;
   }, 2000 );
-
-  $scope.goSpecies = function(){
-     $scope.activeMenu = $scope.userID;
-     $location.path('/species/'+$scope.userID);
-  };
 
 }]);
 
@@ -120,7 +151,7 @@ app.controller('SpeciesCtrl', ['Page','$routeParams','$http','$location','$scope
 }]);
 
 app.controller('MotionCtrl', ['Vote','Page','$rootScope','$routeParams','$http','$location','$scope',function(Vote,Page,$rootScope,$routeParams, $http, $location, $scope){
-  Page.setTitle('Vote for a Motion');
+  Page.setTitle('Start the Game');
   $scope.submitMotion = function(){
     console.log($scope.influence);
   };
